6e2023a40228a614fb4b8bda0a0e3f1fe10e3d45
unnirajeev96 <krishnanunnir9@gmail.com>
2017-01-07 23:05:59 +0530

    Add files via upload

---
+++ src/myapp.js
@@ -0,0 +1,9 @@
+var page = new tabris.Page({
+  topLevel: true,
+  title: "myapp"
+});
+new tabris.TextView({
+  layoutData: {centerX: 0, centerY: 0},
+  text: "My First App"
+}).appendTo(page);
+page.open()
